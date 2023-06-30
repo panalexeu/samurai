33a8de4bd04ba36a6daa48177fcccab2a0fefa55
panalexeu <pan.alexeu@gmail.com>
2023-06-30 23:16:07 +0300

    attempts of adding jumping

--- player.py
+++ player.py
@@ -8,6 +8,8 @@ class Player(sprite.Sprite):
         super().__init__(pos, size_x, size_y, color)
 
         self.speed = 1
+        self.gravity = 1
+        self.jump_speed = 8
 
         self.direction = pygame.math.Vector2(0, 0)  # vector used for movement handling
 
@@ -22,10 +24,20 @@ class Player(sprite.Sprite):
         elif keys[pygame.K_a]:
             self.direction.x = -1
         else:
-            self.direction.x, self.direction.y = 0, 0
+            self.direction.x = 0
+
+        if keys[pygame.K_SPACE]:
+            self.direction.y = -1
+        else:
+            self.direction.y = 0
+
+    def apply_gravity(self):
+        self.rect.y += self.gravity
 
     def update(self):
         self.get_input()
 
         self.rect.x += self.direction.x * self.speed
-        self.rect.y += self.direction.y * self.speed
+        self.rect.y += self.direction.y * self.jump_speed
+
+        self.apply_gravity()
